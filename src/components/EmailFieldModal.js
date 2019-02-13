d6539d2c3e5b267a609ad508b61b0d2446040fed
artze <arthurwtm@gmail.com>
2019-02-13 14:16:51 +0000

    Convert EmailFieldModal to class based component

--- src/components/EmailFieldModal.js
+++ src/components/EmailFieldModal.js
@@ -3,29 +3,33 @@ import Modal from 'react-modal'
 
 Modal.setAppElement('#app')
 
-const EmailFieldModal = (props) => (
-    <Modal
-        isOpen={props.emailFieldModal}
-        className="modal"
-        onRequestClose={props.closeEmailFieldModal}
-    >
-        <div className="modal-content">
-            <label
-                className="label is-large"
-                style={{
-                    width: '100%'
-                }}
-            >Email</label>
-            <input className="input is-large" type="text" placeholder="Type your email here" />
-            <button
-                className={`button is-medium is-success ${props.emailLoading && 'is-loading'}`}
-                onClick={props.submitImage}
-                style={{
-                    'margin-top': '1em'
-                }}
-            >Send</button>
-        </div>
-    </Modal>
-)
+class EmailFieldModal extends React.Component {
+    render() {
+        return (
+            <Modal
+                isOpen={this.props.emailFieldModal}
+                className="modal"
+                onRequestClose={this.props.closeEmailFieldModal}
+            >
+                <div className="modal-content">
+                    <label
+                        className="label is-large"
+                        style={{
+                            width: '100%'
+                        }}
+                    >Email</label>
+                    <input className="input is-large" type="text" placeholder="Type your email here" />
+                    <button
+                        className={`button is-medium is-success ${this.props.emailLoading && 'is-loading'}`}
+                        onClick={this.props.submitImage}
+                        style={{
+                            marginTop: '1em'
+                        }}
+                    >Send</button>
+                </div>
+            </Modal>
+        )
+    }
+}
 
 export default EmailFieldModal
